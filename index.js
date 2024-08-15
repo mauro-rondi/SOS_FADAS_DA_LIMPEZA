f17d587b0c38c211859747542273b9da8c2fd92f
Mauro Rondi <124959570+mauro-rondi@users.noreply.github.com>
2024-08-15 02:52:46 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -22,3 +22,15 @@ document.addEventListener("DOMContentLoaded", function() {
         currentLocation.textContent = `Localização: ${selectedLocation}`;
     });
 });
+
+$(document).ready(function(){
+    $('.image-carousel').slick({
+        dots: true, /* Adiciona indicadores de página */
+        infinite: true, /* Loop infinito */
+        speed: 500, /* Velocidade da transição */
+        slidesToShow: 1, /* Quantidade de slides visíveis */
+        slidesToScroll: 1, /* Quantidade de slides a rolar */
+        autoplay: true, /* Ativa o autoplay */
+        autoplaySpeed: 3000 /* Velocidade do autoplay */
+    });
+});
