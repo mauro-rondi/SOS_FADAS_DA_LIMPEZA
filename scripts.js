13042375a20d6fb52ab9866fbf3c19a746c9eaf9
Mauro Rondi <124959570+mauro-rondi@users.noreply.github.com>
2024-08-22 09:36:16 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 document.getElementById('menu-toggle').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('open');
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+});
+
+// Fechar o menu ao clicar em um link
+var menuLinks = document.querySelectorAll('#menu ul li a');
+menuLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        document.getElementById('menu').classList.remove('show');
+    });
 });
